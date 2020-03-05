61c008fc75f07277fdb067057a3ca9eded4ad6c7
EmmaCrothers <emma.crothers@bbc.co.uk>
2020-03-05 10:30:06 +0000

    add test for all cats and dogs

--- src/__tests__/vet.test.js
+++ src/__tests__/vet.test.js
@@ -10,8 +10,9 @@ describe('vet.js', () => {
     })
 
     it('will return all the cats and dogs in the records', () => {
-        // This is a stub. We will want to return a list of all the cats and dogs,
-        // rather than the boolean value below
-        expect(vet.getCatsAndDogs()).toEqual(true);
+        const result = vet.getCatsAndDogs()
+        const expected = [{"animal": {"age": 11, "gender": "female", "name": "Alysa", "type": "Dog"}, "fee": 449, "id": 1, "owner": {"first_name": "Giovanna", "last_name": "Boughey"}}, {"animal": {"age": 24, "gender": "male", "name": "Carl", "type": "Cat"}, "fee": 697, "id": 2, "owner": {"first_name": "Andrew", "last_name": "Carlson"}}, {"animal": {"age": 25, "gender": "female", "name": "Cary", "type": "Cat"}, "fee": 799, "id": 4, "owner": {"first_name": "Maurie", "last_name": "Dunnion"}}, {"animal": {"age": 25, "gender": "male", "name": "Ira", "type": "Cat"}, "fee": 205, "id": 10, "owner": {"first_name": "Ainsley", "last_name": "Bartak"}}, {"animal": {"age": 22, "gender": "female", "name": "Vick", "type": "Dog"}, "fee": 440, "id": 15, "owner": {"first_name": "Thaddus", "last_name": "Wood"}}, {"animal": {"age": 10, "gender": "female", "name": "Chanda", "type": "Dog"}, "fee": 1289, "id": 19, "owner": {"first_name": "Melly", "last_name": "Rockhall"}}]
+        expect(result).toHaveLength(6)
+        expect(result).toEqual(expected);
     });
 })
--- src/vet.js
+++ src/vet.js
@@ -1,26 +1,21 @@
 // Note: We can tidy and refactor the way that we load records here, and avoid using FS, and the JSON.parse method.
 // But this is also an appropriate way of loading the data in :)
-const fs = require ('fs');
-const data = fs.readFileSync(`${__dirname}/data/records.json`);
+const data = require ('./data/records.json')
 
 const loadRecords = () => {
-    try {
-       return JSON.parse(data);
-    }
-    catch (error) {
-        console.log(error);
-        return []; // We return a sensible default to ensure that the code doesn't crash.
-    }
+  return data
 }
 
 const getCatsAndDogs = () => {
     const records = loadRecords();
 
     if (records.length >= 1){
-        return true;
+        return records.filter((record) =>  {
+            return record.animal.type==='Dog'|| record.animal.type==='Cat'
+        })
     }
     else {
-        return false;
+        return []
     }
 }
 
